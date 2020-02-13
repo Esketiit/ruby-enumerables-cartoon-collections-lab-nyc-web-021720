0a4f9d7aec878fe1f78bbaac6bf6b9173dc8d24f
Brian Desplantes <heftygac@gmail.com>
2020-02-13 15:48:51 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,14 +3,29 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  new_arr = array.map do {|str| str[0].upcase]}
+	array.collect {|str|
+	str[0].upcase + str[1..-1] + "!"
+}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.each {|word| 
+    if (word.length > 4) 
+      return true
+    end
+  }
+  
+  return false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+	array.each{|word|
+		if cheese_types.include?(word)
+			return word
+		end
+	}
+
+	return nil
 end
